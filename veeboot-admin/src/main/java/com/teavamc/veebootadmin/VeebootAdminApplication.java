84664e72ec4c66a9041c95d91f5ea8a4a438a25f
teavamc <teavamc@163.com>
2019-05-05 22:46:21 +0800

    第一次提交

---
+++ veeboot-admin/src/main/java/com/teavamc/veebootadmin/VeebootAdminApplication.java
@@ -0,0 +1,13 @@
+package com.teavamc.veebootadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeebootAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeebootAdminApplication.class, args);
+    }
+
+}
